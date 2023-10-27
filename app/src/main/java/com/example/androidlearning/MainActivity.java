293076ef10f11a6bbf57a6001a12dfcd1afedd9c
LANG XU <44073@kristin.school.nz>
2023-10-28 10:49:21 +1300

    Test5

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -15,31 +15,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setButton();
+        setButton2();
     }
 
-    public void setButton(){
-        Button button1= findViewById(R.id.button09_id);
-        Button button2= findViewById(R.id.button10_id);
-        Button button3= findViewById(R.id.button11_id);
+//    public void setButton(){
+//        Button button1= findViewById(R.id.button09_id);
+//        Button button2= findViewById(R.id.button10_id);
+//        Button button3= findViewById(R.id.button11_id);
+//
+//        button1.setOnClickListener(v->{
+//            button1.setBackgroundColor(getResources().getColor(R.color.red));
+//            button2.setBackgroundColor(getResources().getColor(R.color.black));
+//            button3.setBackgroundColor(getResources().getColor(R.color.black));
+//        });
+//
+//        button2.setOnClickListener(v->{
+//            button2.setBackgroundColor(getResources().getColor(R.color.yellow));
+//            button1.setBackgroundColor(getResources().getColor(R.color.black));
+//            button3.setBackgroundColor(getResources().getColor(R.color.black));
+//        });
+//
+//        button3.setOnClickListener(v->{
+//            button3.setBackgroundColor(getResources().getColor(R.color.green));
+//            button1.setBackgroundColor(getResources().getColor(R.color.black));
+//            button2.setBackgroundColor(getResources().getColor(R.color.black));
+//        });
+//    }
 
-        button1.setOnClickListener(v->{
-            button1.setBackgroundColor(getResources().getColor(R.color.red));
-            button2.setBackgroundColor(getResources().getColor(R.color.black));
-            button3.setBackgroundColor(getResources().getColor(R.color.black));
+    public void setButton2(){
+        Button button = findViewById(R.id.button13_id);
+        button.setOnClickListener(v->{
+            button.setBackgroundColor(getResources().getColor(R.color.white));
+            button.setText(R.string.text_03);
         });
 
-        button2.setOnClickListener(v->{
-            button2.setBackgroundColor(getResources().getColor(R.color.yellow));
-            button1.setBackgroundColor(getResources().getColor(R.color.black));
-            button3.setBackgroundColor(getResources().getColor(R.color.black));
-        });
 
-        button3.setOnClickListener(v->{
-            button3.setBackgroundColor(getResources().getColor(R.color.green));
-            button1.setBackgroundColor(getResources().getColor(R.color.black));
-            button2.setBackgroundColor(getResources().getColor(R.color.black));
-        });
     }
 
 
