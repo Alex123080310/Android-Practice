3bd5680671e1b7c210d58617d4f528bac079f9a2
LANG XU <44073@kristin.school.nz>
2023-11-18 10:03:47 +1300

    Test05

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -17,38 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView02();
-    }
-
-//    private void initView(){
-//        EditText editText = findViewById(R.id.editText01_id);
-//        Button button = findViewById(R.id.button14_id);
-//        Button button2 = findViewById(R.id.button15_id);
-//        button.setOnClickListener(v->{
-//           editText.setText(R.string.text_01);
-//        });
-//
-//        button2.setOnClickListener(v->editText.setText(R.string.empty)
-//        );
-//
-//    }
-
-    private void initView02() {
-        EditText editText = findViewById(R.id.edittext02_id);
-        Button button = findViewById(R.id.button16_id);
-        Button button1 = findViewById(R.id.button16_id3);
-        button.setOnClickListener(v -> {
-            editText.setText(R.string.text_01);
-            button1.setBackgroundColor(getResources().getColor(R.color.black));
-
-
-
-        });
-
-
 
     }
+
+
+
 }
+
+
+
+
+
+
 
 
 
